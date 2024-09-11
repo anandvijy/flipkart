7f237174ef5d2878e057143514ae51d70ec999dc
andvij <anandvijaysharma626@gmail.com>
2024-09-11 20:11:26 +0530

    s

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Data() {
-  return (
+  return(
     <div>
-      <h1></h1>
+      <h1 style={{width:"100px", textAlign:"justify"}}>Data is rjesfkk qwkjdhk jwgdjhudw  wejweh ewjgewjg wejwejg jggwe gg</h1>
     </div>
   )
 }
